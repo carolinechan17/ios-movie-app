4bf638b5ae651cd57f8c0ec868d0ab29fbbb1a6c
Caroline Chan <carolinechan304@gmail.com>
2022-08-18 16:56:58 +0700

    Add: navigation to get to another page

--- movie-app/movie-app/View/HomeView.swift
+++ movie-app/movie-app/View/HomeView.swift
@@ -9,20 +9,22 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Button(action: {}) {
-            HStack {
-                Image(systemName: "film")
-                    .resizable()
-                    .frame(width: 40, height: 40)
-                    .foregroundColor(Color.white)
-                    
-                Text("Movie App")
-                    .fontWeight(.bold)
-                    .foregroundColor(Color.white)
+        NavigationView {
+            NavigationLink(destination: ContentView()) {
+                    HStack {
+                        Image(systemName: "film")
+                            .resizable()
+                            .frame(width: 40, height: 40)
+                            .foregroundColor(Color.white)
+                            
+                        Text("Movie App")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.white)
+                    }
+                    .padding()
+                    .background(Color("Blue2Color"))
+                    .cornerRadius(20)
             }
-            .padding()
-            .background(Color("Blue2Color"))
-            .cornerRadius(20)
         }
     }
 }
