fe65525c0defada5af985a88d311760fbcf9870d
Caroline Chan <carolinechan304@gmail.com>
2022-08-18 15:27:49 +0700

    First commit

---
+++ movie-app/movie-app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  movie-app
+//
+//  Created by Caroline Chan on 18/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ movie-app/movie-app/movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_appApp.swift
+//  movie-app
+//
+//  Created by Caroline Chan on 18/08/22.
+//
+
+import SwiftUI
+
+@main
+struct movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
