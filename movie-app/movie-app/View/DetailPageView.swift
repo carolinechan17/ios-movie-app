3cc227860f513fd1fae3ad2ffc9eba080d613228
Caroline Chan <carolinechan304@gmail.com>
2022-08-22 12:09:09 +0700

    Add detail page view to show detail about movie/tv show

---
+++ movie-app/movie-app/View/DetailPageView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailPageView.swift
+//  movie-app
+//
+//  Created by Caroline Chan on 22/08/22.
+//
+
+import SwiftUI
+
+struct DetailPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DetailPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailPageView()
+    }
+}
